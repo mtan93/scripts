3b490c2ccd7d731b84cf6cc903d77bcba3da2b99
Michael <30936296+mtan93@users.noreply.github.com>
2023-11-26 15:08:27 +0000

    Create br1.sh

---
+++ br1.sh
@@ -0,0 +1,4 @@
+cd /home/print/
+curl -o linux-brprinter-installer-2.2.3-1.gz https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz
+gunzip linux-brprinter-installer-*.*.*-*.gz
+bash linux-brprinter-installer-*.*.*-* DCP-135C
