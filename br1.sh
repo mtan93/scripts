d9c6f7a02c324bf3ed7db6f0d0a21bc44c6f2e09
Michael <30936296+mtan93@users.noreply.github.com>
2023-11-26 16:03:40 +0000

    Update br1.sh

--- br1.sh
+++ br1.sh
@@ -1,5 +1,4 @@
 cd /home/print/
-sudo aa-complain cupsd
 sudo mkdir -p /usr/share/cups/model
 curl -o linux-brprinter-installer-2.2.3-1.gz https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz
 gunzip linux-brprinter-installer-*.*.*-*.gz
