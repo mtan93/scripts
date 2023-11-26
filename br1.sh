5b8037059f70dbf4b5fc289b494e51e08d25c412
Michael <30936296+mtan93@users.noreply.github.com>
2023-11-26 15:43:26 +0000

    Update br1.sh

--- br1.sh
+++ br1.sh
@@ -1,4 +1,6 @@
 cd /home/print/
+sudo aa-complain cupsd
+sudo mkdir -p /usr/share/cups/model
 curl -o linux-brprinter-installer-2.2.3-1.gz https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz
 gunzip linux-brprinter-installer-*.*.*-*.gz
 bash linux-brprinter-installer-*.*.*-* DCP-135C
